11d31dcaa1194df105f0e88b6e718919a58c76b8
lominoss-git <79030093+lominoss-git@users.noreply.github.com>
2024-09-18 17:12:46 +0100

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,20 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-	{
-	    "<leader>f",
-	    function() require("telescope.builtin").find_files() end,
+    {
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+	    {
+			"<leader>f",
+			function() require("telescope.builtin").find_files() end,
+	    },
 	},
-    },
-    opts = {
-        defaults = {
-	    layout_config = { prompt_position = "top" },
-	    sorting_strategy = "ascending",
-            prompt_prefix = "λ ",
-            borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-        },
+	opts = {
+	    defaults = {
+		layout_config = { prompt_position = "top", width = 0.7, height = 0.7 },
+		sorting_strategy = "ascending",
+		prompt_prefix = "λ ",
+		borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+	    },
+	},
     },
 }
